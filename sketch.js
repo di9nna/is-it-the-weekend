52d526068d7a71dd85534755bb68fa23ddedf4e5
Dianna Armenta <diannaa55995@students.laalliance.org>
2017-11-17 20:27:28 +0000

    I'm done with my lab.

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var answer = 'NO'
+
+var weekday = nowArray [0]
+var month = nowArray [1]
+var dayNumber = nowArray [2]
+var year = nowArray [3]
+var time = nowArray [4]
+    if (weekday =  )
+
+
+function setup(){
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw(){
+    background('dodgerblue')
+    textSize(32)
+    fill('white')
+    text('Is it the weekend ' + answer, 40, 40)
+}
